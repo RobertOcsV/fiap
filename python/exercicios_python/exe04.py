7620117c1c8bfbead4bc938978fc884fcdba768f
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-03-19 20:08:04 -0300

    Finalização dos exercicios 4,5 e 6.

---
+++ python/exercicios_python/exe04.py
@@ -0,0 +1,7 @@
+x = input("Digite a base:  ")
+y = input("Digite o expoente: ")
+
+
+x = int(x) ** int(y)
+
+print("o resultado da potência é: ", str(x))
---
+++ python/exercicios_python/exe05.py
@@ -0,0 +1,9 @@
+
+raio = input("Insira o radio do circulo: ")
+
+area_circulo = 3.14 * int(raio) ** 2
+
+print ("A area do circulo é: ", float(area_circulo))
+
+
+
