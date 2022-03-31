e57f45f8317c884cdb7afe2936cce2e0a83680c9
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-03-31 20:46:17 -0300

    mais exercicios python

---
+++ python/exercicios_python/exe06.py
@@ -0,0 +1,7 @@
+inteiro = int(input("Insira um numero de 1 a 99: "))
+
+resto = inteiro  % 10
+
+inteiro = inteiro // 10
+
+print("A divisão inteira é: ", inteiro,"e o resto dela é: ", resto)
---
+++ python/exercicios_python/exe07.py
@@ -0,0 +1,8 @@
+valor1 = int(input("Digite a quantidade de camisetas: "))
+valor2 = int(input("Digite a quantidade de sapatos: "))
+valor3 = int(input("Digite a quantidade de calças"))
+
+valor1 = valor1 * valor2 * valor3
+
+print("O total de possibilidades é: ", valor1)
+
