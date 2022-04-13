00383971c7592746f1884bfb931b5256ac5e80c4
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-04-12 22:38:44 -0300

    Finalizando exercicio 34 de atributos e classe em java

--- java/exercicios_java/src/classe_atributo/exe34.java
+++ java/exercicios_java/src/classe_atributo/exe34.java
@@ -8,9 +8,18 @@ public class exe34 {
 		
 		Scanner ler = new Scanner(System.in);
 		
+		Pessoa[] listaPessoa = new Pessoa[2];
 		
+		char possuiConta;
 		
-		
+		for(int i=0; i>2; i++){
+			
+			Pessoa p;
+			
+			System.out.printf("");
+			
+			
+		}
 		
 		
 
