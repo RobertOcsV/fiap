05564f7f3eb0139e372be81e77b6d445022ac5ef
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-04-12 22:39:47 -0300

    revisado exercicio 10

--- python/exercicios_python/exe10.py
+++ python/exercicios_python/exe10.py
@@ -1,7 +1,12 @@
-valor1 = int(input("Insira o salário base: "))
-valor2 = int(input("Insira o aumento do salário: "))
+
+valor1 = float(input("Insira o salário base: "))
+valor2 = float(input("Insira o aumento do salário: "))
 
 
 valor2 = valor2 - valor1
 
 print("Aumento em dinheiro foi: ", valor2)
+
+valor2 = (valor2 * 100) / valor1
+
+print("O aumento porcentual do salário é: {:.2f}".format(valor2) + "%")
