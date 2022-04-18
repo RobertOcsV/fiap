e88b22d1e1ee911a0a3431aca85fbdd43be8ee9f
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-04-18 18:07:31 -0300

    update exe11 python

---
+++ python/exercicios_python/exe11.py
@@ -0,0 +1,7 @@
+rmv1 = int(input("Insira o primeiro digito do rm: "))
+rmv2 = int(input("Insira o Segundo digito do rm: "))
+rmv3 = int(input("Insira o Terceiro digito do rm: "))
+rmv4 = int(input("Insira o Quarto digito do rm: "))
+rmv5 = int(input("Insira o Quinto digito do rm: "))
+total = rmv1 + rmv2 + rmv3 + rmv4 + rmv5
+print("O somatória do seu rm deve ser (28) ele é: ", total)
