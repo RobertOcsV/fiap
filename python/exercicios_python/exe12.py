6082d2dce5beb9cf8f21d93e42df19ef6fef0e05
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-04-18 18:07:52 -0300

    create exe12 python
    criado e finalizado exercicios 12 de python

---
+++ python/exercicios_python/exe12.py
@@ -0,0 +1,7 @@
+nac = float(input("Insira a nota NAC: "))
+am = float(input("Insira a nota AM: "))
+ps = float(input("Insira a nota PS: "))
+
+media = ((2 * nac) + (3 * am) + (5 * ps)) /10
+
+print("a media foi: ", media)
